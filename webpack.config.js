353b413c4c95add8aade6b54d20c60cc6bff90a1
who <24455370@qq.com>
2025-12-06 17:31:02 +0800

    feat: eslint-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const ESLintPlugin = require('eslint-webpack-plugin');
+
 /**
  * Webpack 配置对象
  * @type {import('webpack').Configuration}
@@ -12,12 +14,20 @@ const config = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: [['@babel/preset-env'], ['@babel/preset-react']],
+            presets: [
+              ['@babel/preset-env'],
+              ['@babel/preset-react', { runtime: 'classic' }],
+            ],
           },
         },
       },
     ],
   },
+  plugins: [
+    new ESLintPlugin({
+      extensions: ['.js', '.jsx'],
+    }),
+  ],
 };
 
 module.exports = config;
