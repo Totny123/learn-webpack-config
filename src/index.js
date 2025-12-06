e2acd050690f569e92e090f203cfc03eb1c19cda
who <24455370@qq.com>
2025-12-06 19:49:02 +0800

    feat: 支持tsx

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { a } from './a.js';
 import { JSXDemo } from './jsx-demo.jsx';
+import { TSXDemo } from './tsx-demo.tsx';
 import { typescriptStr } from './ts-demo.ts';
 const b = import('./b.js');
 
 console.log(typescriptStr);
 console.log(JSXDemo);
+console.log(TSXDemo);
 const hi = () => {
   console.log(a);
   console.log(b);
---
+++ src/tsx-demo.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const TSXDemo = () => <div>TSXDemo</div>;
+
+export { TSXDemo };
