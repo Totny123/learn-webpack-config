0a1012f0c2383c25891233d9a0770c0e28ebe391
who <24455370@qq.com>
2025-12-07 14:37:08 +0800

    feat: less分享变量给js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import { typescriptStr } from './ts-demo.ts';
 import '@/scss-demo.scss';
 import '@/less-demo.less';
 import styles from '@/scss-exports.scss';
+import vars from '@/less-vars.less';
 const b = import('./b.js');
 
 console.log('feng', styles);
+console.log('feng vars', vars);
 console.log(typescriptStr);
 console.log(JSXDemo);
 console.log(TSXDemo);
